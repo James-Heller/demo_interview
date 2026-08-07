d84d7d16f04d24f0d318baedbac0d0c7f7371a80
JamesTang <James-Heller@Outlook.com>
2026-08-07 08:43:56 +0800

    update

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -18,13 +18,16 @@ class DemoApplicationTests {
 
 		/**
 		 *
-		 * 输出应该是SomeData(a=1，b=数组[1,2,3]),SomeData(a= 1，b= 布尔 false)这种格式的。
+		 *
 		 * 你可以随意使用网页版AI。
-		 * *你可以从IDEA自带的数据库工具中查看该表结构与数据。*
+		 * *你可以从IDEA自带的数据库工具中查看该表结构与数据。
+		 * 控制台应有如下输出：
+		 * Foo(id=3, createdAt=2026-07-16T12:45:19.505727, someData=SomeData[a=1, b=数字 3])
+		 * Foo(id=2, createdAt=2026-07-16T12:45:08.784894, someData=SomeData[a=1, b=布尔 false])
+		 * Foo(id=1, createdAt=2026-07-16T12:44:45.323684, someData=SomeData[a=1, b=字符串 AAA])
+		 * Foo(id=4, createdAt=2026-07-16T12:45:31.827420, someData=SomeData[a=1, b=数组 [1,2,3,4])
 		 */
-		mapper.selectList(null).forEach((foo -> {
-			foo.getSomeData().toString();
-		}));
+		mapper.selectList(null).forEach(System.out::println);
 	}
 
 }
